fb57776fb0330fc0e845b667bef2820789925229
3104yoshi <3104adgjmp@gmail.com>
2024-01-29 23:59:30 +0900

    add sample controller

--- api/src/main/java/com/example/api/controller/ItemPriceController.java
+++ api/src/main/java/com/example/api/controller/ItemPriceController.java
@@ -1,14 +1,13 @@
-//package com.example.api.controller;
-//
-//import com.example.calculator.ItemPriceCalculator;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class ItemPriceController {
-//
-//    @GetMapping("/getTotalPrice")
-//    public int getTotalPrice() {
-//        return 1;
-//    }
-//}
+package com.example.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemPriceController {
+
+    @GetMapping("/getTotalPrice")
+    public int getTotalPrice() {
+        return 1;
+    }
+}
